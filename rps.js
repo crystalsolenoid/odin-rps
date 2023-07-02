6b9c26d69c2dffa5bdee929bf33f8889178de6e2
Quinten Konyn <qkonyn@gmail.com>
2023-07-02 02:14:52 -0700

    Add playRound function: logic for what beats what

--- rps.js
+++ rps.js
@@ -1,10 +1,45 @@
 function getComputerChoice() {
   switch (Math.floor(Math.random() * 3)) {
     case 0:
-      return "rock";
+      return 'rock';
     case 1:
-      return "paper";
+      return 'paper';
     case 2:
-      return "scissors";
+      return 'scissors';
+  }
+}
+
+// utility function that capitalizes only the first letter of a string,
+// making the rest lowercase
+let capitalize = s => s.slice(0,1).toUpperCase() + s.slice(1).toLowerCase();
+
+function playRound(playerSelection, computerSelection) {
+  // clean player input. should be refactored later.
+  playerSelection = playerSelection.toLowerCase();
+  // handle ties first because they're easy.
+  if (playerSelection === computerSelection) {
+    return `It's a tie! Both ${playerSelection}.`;
+  }
+  // to handle the rest, first identify if the player wins or loses,
+  // then remember and build the message from that.
+  let result;
+  // just smoosh the strings together to handle them with a switch instead of nested ifs
+  switch (playerSelection + computerSelection) {
+    case 'rockpaper':
+    case 'paperscissors':
+    case 'scissorsrock':
+      result = 'loss';
+      break;
+    case 'paperrock':
+    case 'scissorspaper':
+    case 'rockscissors':
+      result = 'win';
+      break;
+  }
+  // knowing who chose what an if it's a loss or win, we can now build the messages
+  if (result === 'loss') {
+    return `You lose! ${capitalize(computerSelection)} beats ${playerSelection}.`;
+  } else {
+    return `You win! ${capitalize(playerSelection)} beats ${computerSelection}.`;
   }
 }
